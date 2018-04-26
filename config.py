12a07474bcf56b2d6c554d203492bbcde1b745b5
kyradornish <kyradornish@gmail.com>
2018-04-26 12:17:29 -0400

    changed to sqlite db

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://kyra:ladies1271@localhost:5433/recipe_spinner'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
 
     SECRET_KEY = 'super-secret'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
